d1f67fe856ac105d201f8c4aa8870a7a17005076
Michał Skwara <michal.skwara@fingoweb.com>
2022-10-09 13:11:45 +0200

    [PCS-127] Remove mocked data

--- src/components/Map/consts.js
+++ src/components/Map/consts.js
@@ -5,28 +5,4 @@ const anchorPoint = {
 
 const zoom = 12;
 
-const mockedMarkers = [
-  {
-    position: {
-      lat: 50.061,
-      lng: 19.943,
-    },
-    radius: 200,
-  },
-  {
-    position: {
-      lat: 50.064799,
-      lng: 19.932365,
-    },
-    radius: 500,
-  },
-  {
-    position: {
-      lat: 50.066296,
-      lng: 19.958939,
-    },
-    radius: 2000,
-  },
-];
-
-export { anchorPoint, zoom, mockedMarkers };
+export { anchorPoint, zoom };
