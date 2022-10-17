1a4ab0b2e8e585e6279908518bced32210623996
Michał Skwara <michal.skwara@fingoweb.com>
2022-10-17 13:28:14 +0200

    [PCS-169] Create Loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Typography } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import { Box } from '@mui/system';
 
 const Loader = () => {
-  const { t } = useTranslation();
-
   return (
     <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-      <Typography>{t('loading')}</Typography>
+      <CircularProgress size={20} thickness={5} />
     </Box>
   );
 };
